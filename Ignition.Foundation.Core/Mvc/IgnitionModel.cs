fc2f496929c9ad71380a7b450cfac0611a8b4c5d
Jon Upchurch <jon.upchurch@perficient.com>
2016-12-27 14:38:44 -0500

    Splitting IgnitionModel out from IgnitionViewModel

---
+++ Ignition.Foundation.Core/Mvc/IgnitionModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Converters;
+using Sitecore.Data;
+using Sitecore.Globalization;
+
+namespace Ignition.Foundation.Core.Mvc
+{
+	public class IgnitionModel
+	{
+		[SitecoreId, IndexField("_group")]
+		public virtual Guid Id { get; set; }
+
+		[SitecoreInfo(SitecoreInfoType.Language), IndexField("_language")]
+		public virtual Language Language { get; set; }
+
+		[TypeConverter(typeof(IndexFieldItemUriValueConverter)), XmlIgnore, IndexField("_uniqueid")]
+		public virtual ItemUri Uri { get; set; }
+
+		[SitecoreInfo(SitecoreInfoType.DisplayName)]
+		public virtual string DisplayName { get; set; }
+
+		[SitecoreInfo(SitecoreInfoType.Version)]
+		public virtual int Version { get; set; }
+
+		[SitecoreInfo(SitecoreInfoType.Path), IndexField("_path")]
+		public virtual string Path { get; set; }
+
+		[SitecoreInfo(SitecoreInfoType.Name), IndexField("_name")]
+		public virtual string Name { get; set; }
+
+		[SitecoreInfo(SitecoreInfoType.Url, UrlOptions = SitecoreInfoUrlOptions.LanguageEmbeddingNever)]
+		public virtual string Url { get; set; }
+	}
+}
--- Ignition.Foundation.Core/Mvc/IgnitionViewModel.cs
+++ Ignition.Foundation.Core/Mvc/IgnitionViewModel.cs
@@ -13,27 +13,12 @@ using Sitecore.Globalization;
 namespace Ignition.Foundation.Core.Mvc
 {
 	[SitecoreType(TemplateId = "{4C3CDC24-1610-4808-92A3-A221768AE3B2}", AutoMap = true)]
-	public class IgnitionViewModel  
+	public class IgnitionViewModel  : IgnitionModel
     {
-        public IPage ContextPage { get; set; }
+
+	    public IPage ContextPage { get; set; }
 		public object GlassCssClassParameters(string cssClass) => new { @class = cssClass };
 	    public object GlassImageParameters(string cssClass, int height, int width) => new { @class = cssClass, height = height.ToString(), width = width.ToString() };
 	    public NameValueCollection GlassAttributeParameters(string attributeName, string attributeValue) => new NameValueCollection { { attributeName, attributeValue } };
-		[SitecoreId, IndexField("_group")]
-		public virtual Guid Id { get; set; }
-		[SitecoreInfo(SitecoreInfoType.Language), IndexField("_language")]
-		public virtual Language Language { get; set; }
-		[TypeConverter(typeof(IndexFieldItemUriValueConverter)), XmlIgnore, IndexField("_uniqueid")]
-		public virtual ItemUri Uri { get; set; }
-		[SitecoreInfo(SitecoreInfoType.DisplayName)]
-		public virtual string DisplayName { get; set; }
-		[SitecoreInfo(SitecoreInfoType.Version)]
-		public virtual int Version { get; set; }
-		[SitecoreInfo(SitecoreInfoType.Path), IndexField("_path")]
-		public virtual string Path { get; set; }
-		[SitecoreInfo(SitecoreInfoType.Name), IndexField("_name")]
-		public virtual string Name { get; set; }
-		[SitecoreInfo(SitecoreInfoType.Url, UrlOptions = SitecoreInfoUrlOptions.LanguageEmbeddingNever)]
-		public virtual string Url { get; set; }
     }
 }
