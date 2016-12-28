926afba3078f9e7f5a7c7c7879e651ee33c7679e
Jon Upchurch <jon.upchurch@perficient.com>
2016-12-28 16:56:25 -0500

    More Clean-up

---
+++ Ignition.Feature.EloquaForm/Controllers/EloquaFormController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ignition.FormIgnition.Sc.Contracts.Form;
+using Ignition.FormIgnition.Sc.Mvc;
+
+namespace Ignition.Feature.EloquaForm.Controllers
+{
+	public class EloquaFormController : IgnitionFormController
+	{
+		protected override IFormConfiguration Configuration { get; set; }
+		protected override IFormAuthentication FormAuthentication { get; set; }
+	}
+}
---
+++ Ignition.Feature.EloquaForm/ViewModels/EloquaFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ignition.Foundation.Core.Mvc;
+
+namespace Ignition.Feature.EloquaForm.ViewModels
+{
+	public class EloquaFormViewModel : IgnitionViewModel
+	{
+	}
+}
