1594f24c0e0b3274b85b1fcb53773c4a9b623c8a
Corey Smith <coreyas@gmail.com>
2018-01-16 12:13:51 -0500

    Replace Simple Injector with Microsoft Dependency Injection

---
+++ Ignition.Foundation.CompositionRoot/IgnitionConfigurator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ignition.Foundation.CompositionRoot.DependencyInjection;
+using Ignition.Foundation.Core.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Ignition.Foundation.CompositionRoot
+{
+  public class IgnitionConfigurator : IServicesConfigurator
+  {
+    public void Configure(IServiceCollection serviceCollection)
+    {
+      var assemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList();
+
+      //load possible standalone assemblies
+      assemblies.AddRange(AppDomain.CurrentDomain.GetAssemblies()
+        .Where(assembly => assembly.GetCustomAttributes(typeof(IgnitionAutomapAttribute)).Any())
+        .Where(a => !assemblies.Contains(a))
+        .Select(a => Assembly.Load(a.FullName)));
+
+      foreach (var assembly in assemblies)
+      {
+        serviceCollection.AddMvcControllers(assembly);
+
+        var configuratorTypes = GetServicesConfiguratorTypesFromAssembly(assembly);
+        foreach (var configuratorType in configuratorTypes)
+        {
+          var configurator = (IServicesConfigurator)Activator.CreateInstance(configuratorType);
+          configurator.Configure(serviceCollection);
+        }
+      }
+    }
+
+    private static IEnumerable<Type> GetServicesConfiguratorTypesFromAssembly(Assembly assembly)
+    {
+      var configuratorTypes = assembly.ExportedTypes.Where(et => typeof(IServicesConfigurator).IsAssignableFrom(et))
+        .Where(et => !et.IsAbstract)
+        .Where(et => !et.IsGenericTypeDefinition);
+      return configuratorTypes;
+    }
+  }
+}
---
+++ Ignition.Foundation.Core/IgnitionCoreConfigurator.cs
@@ -0,0 +1,16 @@
+﻿using Ignition.Foundation.Core.Factories;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Ignition.Foundation.Core
+{
+  public class IgnitionCoreConfigurator : IServicesConfigurator
+  {
+    public void Configure(IServiceCollection serviceCollection)
+    {
+      serviceCollection.AddScoped<ISitecoreServiceFactory, SitecoreServiceFactory>();
+      serviceCollection.AddScoped<IIgnitionControllerContextFactory, IgnitionControllerContextFactory>();
+      serviceCollection.AddTransient<ISitecoreSettingsFactory, SitecoreSettingsFactory>();
+    }
+  }
+}
